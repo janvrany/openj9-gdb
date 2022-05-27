e7f0968a15a28bdaee160b686199340efdc4e4b7
Jan Vrany <jan@vrany.io>
2022-05-27 12:48:11 +0100

    Remove autodebug support
    ...as it has been moved to vdb.

--- openj9/__init__.py
+++ openj9/__init__.py
@@ -34,25 +34,4 @@ if __registrar == None:
     __registrar = MethodInfoRegistrar()
 
 
-# Install automatic debugging hook
-def autodebug(typ, value, tb):
-    if False: # hasattr(sys, 'ps1') or not sys.stderr.isatty():
-        # we are in interactive mode or we don't have a tty-like
-        # device, so we call the default hook
-        sys.__excepthook__(typ, value, tb)
-    else:
-        import traceback, pdb
-        # we are NOT in interactive mode, print the exception...
-        if tb is None:
-            print("EXECEPTION IN PYTHON CODE:")
-            traceback.print_last()
-        else:
-            print("EXECEPTION IN PYTHON CODE, ENTERING DEBUGGER:")
-            traceback.print_exception(typ, value, tb)
-            # ...then start the debugger in post-mortem mode.
-            # pdb.pm() # deprecated
-            pdb.post_mortem(tb) # more "modern"
-
-sys.excepthook = autodebug
-
 
